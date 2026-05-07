f4e002ae70e7b6c2c467d705b56f750b0b1f38f1
samruddhi-builds <samruddhidave13@gmail.com>
2026-05-07 09:52:07 +0530

    Implement prime number check in primes.py
    Fix logic to correctly identify prime numbers.

---
+++ primes.py
@@ -0,0 +1,8 @@
+num= int(input("Enter the prime number"))
+
+if num>1:
+        for i in range(n):
+          if num % i==0:
+            print("This is a Prime number")
+else:
+print("Not a prime number")
