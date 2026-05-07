a82c7b589644898bd1b2831f8aebffb8a64fcc06
samruddhi-builds <samruddhidave13@gmail.com>
2026-05-07 09:53:43 +0530

    Fix prime number check logic and formatting

--- primes.py
+++ primes.py
@@ -4,5 +4,5 @@ if num>1:
         for i in range(n):
           if num % i==0:
             print("This is a Prime number")
-else:
-print("Not a prime number")
+        else:
+                print("Not a prime number")
